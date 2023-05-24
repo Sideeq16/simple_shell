b88f805cbd84ce5ed856edbd0aebff38db58c820
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-05-24 11:58:57 +0100

    Update shell.c

---
+++ shell.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+  char *command;
+  int status;
+
+  while (1) {
+    printf("$ ");
+    fflush(stdout);
+
+    command = malloc(1024);
+    getline(command, 1024, stdin);
+
+    // Check for end of file condition
+    if (feof(stdin)) {
+      free(command);
+      break;
+    }
+
+    // Strip the newline character from the end of the command
+    command[strcspn(command, "\n")] = '\0';
+
+    // Try to execute the command
+    pid_t pid = fork();
+    if (pid == 0) {
+      execve(command, NULL, environ);
+      // If execve returns, the command could not be found
+      perror("execve");
+      exit(1);
+    } else {
+      wait(&status);
+    }
+
+    free(command);
+  }
+
+  return 0;
+}
