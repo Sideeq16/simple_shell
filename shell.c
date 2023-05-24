40a35516201a998cde8fad538b8914acfbdd6269
Sideeq16 <ajikuru3@gmail.com>
2023-05-24 14:19:17 +0000

    Added file

--- shell.c
+++ shell.c
@@ -2,23 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 /**
  * main - simple shell program
+ * @ac: count commad args
+ * @av: store command value
  * Return: zero on successful
  */
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
 char *line = NULL;
 int status;
 size_t len = 0;
 ssize_t nread;
 char *argv[2];
-argv[1] = NULL;
 char *err_msg = ": No such file or directory\n";
+argv[1] = NULL;
+(void)ac;
 
-while (1) {
+while (1)
+{
 write(STDOUT_FILENO, "$ ", 2);
 nread = getline(&line, &len, stdin);
-if (nread == -1) {
+if (nread == -1)
+{
 write(STDOUT_FILENO, "\n", 1);
 exit(0);
 }
@@ -31,11 +40,11 @@ write(STDERR_FILENO, av[0], strlen(av[0]));
 write(STDERR_FILENO, err_msg, strlen(err_msg));
 exit(1);
 }
-else 
+else
 {
 wait(&status);
 }
 }
 free(line);
-return 0;
+return (0);
 }
