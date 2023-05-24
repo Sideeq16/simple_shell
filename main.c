06a61cf80b91bff4fbce46130387c91888eac654
Sideeq16 <ajikuru3@gmail.com>
2023-05-24 16:43:51 +0000

    added file

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "main.h"
 
 /**
  * main - simple shell program
@@ -14,28 +15,26 @@
  */
 int main(int argc, char *argv[], char **env)
 {
-	char *inputs = NULL;
 	size_t n = 0;
-	char *args[] = {NULL};
+	char *args[] = {NULL}, *inputs = NULL;
 	pid_t child_proc;
-	int status;
+	int status, is_interative = isatty(STDIN_FILENO);
 	(void)argc;
 
-	/* printf("#cisfun$ ");*/
 	while (1)
 	{
+		if (is_interative)
+		{
 		printf("#cisfun$ ");
 		fflush(stdout);
+		}
 
 		if (getline(&inputs, &n, stdin) == -1)
 			break;
 
 		inputs[strlen(inputs) - 1] = '\0';
-		if (strcmp(inputs, "exit") == 0)
-			break;
-
+		/*if (strcmp(inputs, "exit") == 0)break;*/
 		child_proc = fork();
-
 		if (child_proc == -1)
 		{
 			perror(argv[0]);
@@ -45,12 +44,12 @@ int main(int argc, char *argv[], char **env)
 		{
 			execve(inputs, args, env);
 			perror(argv[0]);
+			free(inputs);
 			exit(1);
 		}
 		else
 		{
 			wait(&status);
-			printf("\n");
 		}
 
 	}
