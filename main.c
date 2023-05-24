4c5e9716161c0868108297a3227c3854dc6e9d20
Sideeq16 <ajikuru3@gmail.com>
2023-05-24 18:21:52 +0000

    Added file

--- main.c
+++ main.c
@@ -1,46 +1,117 @@
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - simple shell program
- * @argc: number of argument
- * @argv: arugment passed
- * Return: zero on successful
+ * handle_interrupt - handles Ctrl+C
+ * @signum: integer
+ * Return: void
  */
-int main(int argc, char *argv[])
+void handle_interrupt(int signum)
 {
-	size_t n = 0;
-	char *inputs = NULL;
-	/*pid_t child_proc;*/
-	int is_interative = isatty(STDIN_FILENO);
-	(void)argc;
+	(void)signum;
+
+}
+/**
+ * exe_cmd1 - executes command without command line argument and path
+ * @command: command to execute
+ * @av: argument vector
+ * Reurn: void
+ */
+void exe_cmd1(char *command, char **av)
+{
+	char *command_path;
+	int status, i;
+	pid_t pid;
+
+	if (strlen(command) == 0 || strspn(command, " \t") == strlen(command))
+	{
+		return;
+	}
+	av = _tokenize(command, " \t");
+	if (av == NULL)
+	{
+		perror("Tokenization error");
+		return;
+	}
+	command_path = find_command(av[0]);
+	if (command_path == NULL)
+	{
+		perror(av[0]);
+		return;
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Fork");
+		free(command_path);
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(command_path, av, NULL) == -1)
+		{
+			perror(av[0]);
+			exit(1);
+		}
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			free(command_path);
+			exit(1);
+		}
+	}
+	for (i = 0; av[i] != NULL; i++)
+	{
+		free(av[i]);
+	}
+	free(av);
+}
+/**
+ * shell_loop - performs the loop operation
+ * @av: argument vector
+ * Return: void
+ */
+void shell_loop(char **av)
+{
+	char *command = NULL;
+	size_t command_length = 0;
+
+	signal(SIGINT, handle_interrupt);
 
 	while (1)
 	{
-		if (is_interative)
-		{
-		write(STDOUT_FILENO, "#cisfun$ ", 9);
+		prompt();
 		fflush(stdout);
-		}
-
-		if (getline(&inputs, &n, stdin) == -1)
+		if (getline(&command, &command_length, stdin) == -1)
 		{
-			free(inputs);
+			free(command);
 			exit(0);
 		}
-
-		/*inputs[strlen(inputs) - 1] = '\0';*/
-		inputs[strcspn(inputs, "\n")] = '\0';
-		run_cmd(inputs, argv);
-		free(inputs);
-
-
+		command[strcspn(command, "\n")] = '\0';
+		exe_cmd1(command, av);
+		free(command);
+		command = NULL;
 	}
-	free(inputs);
+}
+
+/**
+ * main - entry point of the shell
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		exe_cmd1(av[1], av);
+		return (0);
+	}
+	else
+	{
+		shell_loop(av);
+	}
 	return (0);
 }
