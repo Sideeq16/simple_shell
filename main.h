aa7a1ca0e9610aeb1ebd9464c88d6bdff8059d4c
Sideeq16 <ajikuru3@gmail.com>
2023-05-25 15:52:42 +0000

    Added file

--- main.h
+++ main.h
@@ -6,5 +6,6 @@ extern char **environ;
 char **m_token(char *str, const char *delim);
 int _strcmp(char *s1, char *s2);
 void inner_exec(char **argv);
+ssize_t my_getline(char **input_line, size_t *input_line_size, FILE *input_stream);
 
 #endif
