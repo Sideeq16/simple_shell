0d0d1984e5bb5e56d21ac11708f80adbc231023c
Sideeq16 <ajikuru3@gmail.com>
2023-05-25 14:27:05 +0000

    Added files'

--- main.h
+++ main.h
@@ -6,7 +6,5 @@ extern char **environ;
 char **m_token(char *str, const char *delim);
 int _strcmp(char *s1, char *s2);
 void inner_exec(char **argv);
-char *find_command(char *command);
-char *build_path(char *dir, char *command);
 
 #endif
