e029be49f51ec2cc02b061b46cf6eccfd03de8f1
Sideeq16 <ajikuru3@gmail.com>
2023-05-25 21:25:04 +0000

    added file'

--- main.h
+++ main.h
@@ -6,7 +6,8 @@ extern char **environ;
 char **m_token(char *str, const char *delim);
 int _strcmp(char *s1, char *s2);
 void inner_exec(char **argv);
-ssize_t my_getline(char **input_line, size_t *input_line_size, FILE *input_stream);
+ssize_t my_getline(char **input_line, size_t *input_line_size,
+FILE *input_stream);
 char *my_strtok(char *input_str, const char *separator);
 int _atoi(char *s);
 /*static int read_next_char(char *current_char, FILE *input_stream);*/
