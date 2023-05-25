3cac7814a0eb31d3c60735c19bb7b6939e8802e3
Sideeq16 <ajikuru3@gmail.com>
2023-05-25 17:06:09 +0000

    added getline

--- getline.c
+++ getline.c
@@ -1,72 +1,90 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define READ_BUFFER_SIZE 1024
+
 
 static char read_buffer[READ_BUFFER_SIZE];
 static int read_buffer_pos;
 static int read_buffer_len;
 
+/**
+ * read_next_char - read user input
+ * @current_char: current characer
+ * @input_stream: stream
+ * Return: int value
+ */
 static int read_next_char(char *current_char, FILE *input_stream)
 {
-    if (read_buffer_pos >= read_buffer_len)
-    {
-        read_buffer_len = read(fileno(input_stream),
-                               read_buffer,
-                               READ_BUFFER_SIZE);
-        read_buffer_pos = 0;
-    }
+	if (read_buffer_pos >= read_buffer_len)
+	{
+	read_buffer_len = read(fileno(input_stream),
+	read_buffer,
+	READ_BUFFER_SIZE);
+	read_buffer_pos = 0;
+	}
 
-    if (read_buffer_len == 0)
-        return 0;
+	if (read_buffer_len == 0)
+	return (0);
 
-    if (read_buffer_len < 0)
-        return -1;
+	if (read_buffer_len < 0)
+	return (-1);
 
-    *current_char = read_buffer[read_buffer_pos++];
-    return 1;
-}
+	*current_char = read_buffer[read_buffer_pos++];
+	return (1);
+	}
+
+/**
+ * my_getline - get line of text
+ * @input_line: line of input
+ * @input_line_size: size of input line
+ * @input_stream: stream type
+ * Return: int value
+ */
 
 ssize_t my_getline(char **input_line,
-                   size_t *input_line_size,
-                   FILE *input_stream)
+	size_t *input_line_size,
+	FILE *input_stream)
 {
-    if (input_line == NULL || input_line_size == NULL || input_stream == NULL)
-        return -1;
+	unsigned long int input_line_pos = 0;
 
-    if (*input_line == NULL)
-    {
-        *input_line = malloc(READ_BUFFER_SIZE);
-        if (*input_line == NULL)
-            return -1;
-        *input_line_size = READ_BUFFER_SIZE;
-    }
+	if (input_line == NULL || input_line_size == NULL || input_stream == NULL)
+	return (-1);
 
-    int input_line_pos = 0;
-    while (1)
-    {
-        char current_char;
-        int result = read_next_char(&current_char, input_stream);
-        if (result <= 0)
-            break;
+	if (*input_line == NULL)
+	{
+	*input_line = malloc(READ_BUFFER_SIZE);
+	if (*input_line == NULL)
+	return (-1);
+	*input_line_size = READ_BUFFER_SIZE;
+	}
 
-        (*input_line)[input_line_pos++] = current_char;
+	while (1)
+	{
+	char current_char;
+	int result = read_next_char(&current_char, input_stream);
 
-        if (input_line_pos >= *input_line_size)
-        {
-            size_t new_input_line_size = *input_line_size * 2;
-            char *new_input_line =
-                realloc(*input_line, new_input_line_size);
-            if (new_input_line == NULL)
-                return -1;
-            *input_line = new_input_line;
-            *input_line_size = new_input_line_size;
-        }
+	if (result <= 0)
+	break;
 
-        if (current_char == '\n')
-            break;
-    }
+	(*input_line)[input_line_pos++] = current_char;
 
-    (*input_line)[input_line_pos] = '\0';
-    return input_line_pos;
+	if (input_line_pos >= *input_line_size)
+	{
+	size_t new_input_line_size = *input_line_size * 2;
+	char *new_input_line =
+	realloc(*input_line, new_input_line_size);
+	if (new_input_line == NULL)
+	return (-1);
+	*input_line = new_input_line;
+	*input_line_size = new_input_line_size;
+	}
+
+	if (current_char == '\n')
+	break;
+	}
+
+	(*input_line)[input_line_pos] = '\0';
+	return (input_line_pos);
 }
--- main.h
+++ main.h
@@ -7,5 +7,6 @@ char **m_token(char *str, const char *delim);
 int _strcmp(char *s1, char *s2);
 void inner_exec(char **argv);
 ssize_t my_getline(char **input_line, size_t *input_line_size, FILE *input_stream);
+/*static int read_next_char(char *current_char, FILE *input_stream);*/
 
 #endif
