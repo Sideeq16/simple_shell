341fe0023a1319b5b00317582c1a99a54f21e242
Sideeq16 <ajikuru3@gmail.com>
2023-05-26 04:06:13 +0000

    Added file

--- _tokens.c
+++ _tokens.c
@@ -13,14 +13,14 @@ char **m_token(char *str, const char *delim)
 {
 	int token_count = 0;
 	char **tokens = NULL;
-	char *token = my_strtok(str, delim);
+	char *token = strtok(str, delim);
 
 	while (token != NULL)
 	{
 	token_count++;
 	tokens = realloc(tokens, (token_count + 1) * sizeof(char *));
 	tokens[token_count - 1] = token;
-	token = my_strtok(NULL, delim);
+	token = strtok(NULL, delim);
 	}
 
 	tokens[token_count] = NULL;
